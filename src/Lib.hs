82637c005303912f6ad2c24f5d67d2c60a5c35b3
Martin Lab <martinlab at bcm.edu>
2016-02-29 14:17:13 -0600

    v0.1.0

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,59 @@
+{-#LANGUAGE OverloadedStrings#-}
+
+{-
+Project name: Annotate ensembl genes to symbol using v24 gencode human gtf
+Min Zhang
+Date: Feb 26, 2016
+Version: v0.1.0
+README: 
+-}
+
 module Lib
-    ( someFunc
+    ( annotateEnsembl
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as TextIO
+import Control.Applicative
+import qualified Data.List as L
+import Control.Monad (fmap)
+import Data.Ord (comparing)
+import Data.Function (on)
+import qualified Safe as S
+import qualified Data.HashMap.Lazy as M
+import qualified Data.Maybe as Maybe
+import System.Environment (getArgs)
+
+ref filepath = TextIO.readFile filepath >>= return . M.fromList . removeDup . map (T.splitOn " ") . T.lines
+
+refMouse = ref "data/unique.vM8.annotation.pairs.txt" 
+refHuman = ref "data/unique.v24.annotation.pairs.txt"
+
+toPair [x, y] = (x, y)
+
+readInt :: Int -> T.Text
+readInt x = T.pack $ show x
+
+counter [] = []
+counter xs = concat $ map toIndex groupedListLen
+             where groupedListLen = map length $ L.group xs
+                   toIndex x = map readInt [1..x]
+
+
+changeGeneName xs = (take 1 xs) ++ [zipWith addCounter nameCol (counter nameCol)]
+                    where nameCol = head $ drop 1 xs
+                          addCounter a b = if b == "1" then a else T.concat [a, "_", b] 
+
+removeDup xs = map toPair $ L.transpose $ changeGeneName $ L.transpose $ L.sortBy (comparing last) xs
+
+annotateEnsembl = do
+  -- assume id is the first column
+   [species, inputpath] <- take 2 <$> getArgs
+   refmap <- (case species of 
+                    "mm10" -> refMouse
+                    "hg38" -> refHuman
+                    otherwise -> refHuman)
+   result <- T.unlines . map ((\(ensembl, rest) -> T.concat [M.lookupDefault ensembl ensembl refmap, rest]) . (\x->T.breakOn "\t" x)) . T.lines <$> TextIO.readFile inputpath
+   TextIO.putStr result
+   
+
